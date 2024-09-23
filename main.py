67ea589a77320b98d47a3efa729b77cfba00d007
David Hanks <dphanks@gmail.com>
2024-09-23 18:40:53 +0000

    init commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+from alpaca.data.live import StockDataStream
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Your Alpaca API key ID and secret
+API_KEY = os.getenv("ALPACA_API_KEY")
+API_SECRET = os.getenv("ALPACA_API_SECRET")
+
+def live_alpaca_bars(symbol='AAPL'):
+    
+    # Get the OHLCV 1 min bars for the given symbol
+    async def bar_data_handler(data):
+        print(data)    
+    
+    # Subscribe to the live stock data stream
+    wss_client = StockDataStream(API_KEY, API_SECRET)
+    wss_client.subscribe_bars(bar_data_handler, symbol)
+    wss_client.run()
+
+if __name__== "__main__":
+
+    live_alpaca_bars('AAPL')
