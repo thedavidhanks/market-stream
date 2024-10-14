5bfec7f6b235425ad237b8aecf89a1f3c6b6b0b5
David Hanks <dphanks@gmail.com>
2024-10-14 18:49:58 +0000

    corrects env vars in test file.

--- test.py
+++ test.py
@@ -8,11 +8,11 @@ load_dotenv()
 API_KEY = os.getenv("ALPACA_API_KEY")
 API_SECRET = os.getenv("ALPACA_API_SECRET")
 # Database info
-DB_PWD = os.getenv("DB_PWD")
-DB_URL = os.getenv("DB_URL")
-DB_USER = os.getenv("DB_USER")
-DB_NAME = os.getenv("DB_NAME")
-DB_PORT = os.getenv("DB_PORT")
+DB_PWD = os.getenv("MS_DB_PWD")
+DB_URL = os.getenv("MS_DB_URL")
+DB_USER = os.getenv("MS_DB_USER")
+DB_NAME = os.getenv("MS_DB_NAME")
+DB_PORT = os.getenv("MS_DB_PORT")
 
 connection = connect_to_db(DB_USER, DB_PWD, DB_URL, DB_NAME, port=DB_PORT)
 
